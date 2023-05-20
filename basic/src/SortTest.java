6755b3c94cd682500bfa70a1cd86813cf7340206
Mingyu Kim <flyordig@gmail.com>
2023-05-20 22:33:32 +0900

    정렬알고리즘 테스트용 클래스 생성

---
+++ basic/src/SortTest.java
@@ -0,0 +1,16 @@
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		// TODO 정렬 알고리즘 구현
+		// 1. 버블정렬 
+		System.out.println("---버블정렬---");
+		
+		
+		
+		// 2. 순차정렬
+		System.out.println("---순차정렬---");
+
+	}
+
+}
