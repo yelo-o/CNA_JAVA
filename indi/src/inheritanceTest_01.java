c75d82778d84744ddaa26caf04f305f0593ae163
yelo-o <flyordig@gmail.com>
2023-06-03 19:27:21 +0900

    부모 생성자 호출

--- indi/src/inheritanceTest_01.java
+++ indi/src/inheritanceTest_01.java
@@ -1,28 +1,28 @@
-class 부모{
-	int field1;
-	void method1() {
-		System.out.println("int 타입의 field 1 : "+ field1);
-		System.out.println("부모 class로 부터 왔습니다.");
-	}
-}
-class 자식 extends 부모{
-	String field2;
-	void method2() {
-		System.out.println("String 타입의 field 2 : "+ field2);
-		System.out.println("자식 class에서 생성되었습니다.");
-	}
-}
-
-public class inheritanceTest_01 {
-	
-	public static void main(String[] args) {
-		//child 변수에 자식 객체 생성
-		자식 child = new 자식();
-		
-		child.field1 = 10;
-		child.method1();
-		
-		child.field2 = "홍길동";
-		child.method2();
-	}
-}
+//class 부모{
+//	int field1;
+//	void method1() {
+//		System.out.println("int 타입의 field 1 : "+ field1);
+//		System.out.println("부모 class로 부터 왔습니다.");
+//	}
+//}
+//class 자식 extends 부모{
+//	String field2;
+//	void method2() {
+//		System.out.println("String 타입의 field 2 : "+ field2);
+//		System.out.println("자식 class에서 생성되었습니다.");
+//	}
+//}
+//
+//public class inheritanceTest_01 {
+//	
+//	public static void main(String[] args) {
+//		//child 변수에 자식 객체 생성
+//		자식 child = new 자식();
+//		
+//		child.field1 = 10;
+//		child.method1();
+//		
+//		child.field2 = "홍길동";
+//		child.method2();
+//	}
+//}
