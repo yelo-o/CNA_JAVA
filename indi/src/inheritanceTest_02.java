45a7a17cff37be1f5c59054495c145cf24d92a68
yelo-o <flyordig@gmail.com>
2023-06-04 11:01:59 +0900

    메소드오버라이딩 예제1

--- indi/src/inheritanceTest_02.java
+++ indi/src/inheritanceTest_02.java
@@ -1,38 +1,38 @@
-class 부모{
-	String 국적 = "대한민국";
-	int 재산 = 10000;
-	void 부모메서드() {
-		System.out.println("저의 국적은 "+ this.국적 + ", 재산은 " + 재산 + "입니다.");
-	}
-}
-class 자식 extends 부모{
-	String 국적 = "인도";
-	int 재산 = 50000;
-	자식() {
-	}
-	자식(String 국적, int 재산){
-		this.국적 = 국적;
-		super.재산 = 재산;
-	}
-	void 자식메서드() {
-		System.out.println("저의 국적은 "+ super.국적 + ", 재산은 " + super.재산 + "입니다.");
-		System.out.println("저의 국적은 "+ this.국적 + ", 재산은 " + this.재산 + "입니다.");
-	}
-}
-
-public class inheritanceTest_02 {
-	
-	public static void main(String[] args) {
-		//부모님의 국적과 재산을 그대로 물려받은 child1
-		자식 child1 = new 자식();
-		child1.부모메서드();
-		child1.자식메서드();
-		System.out.println("=================================");
-		
-		//물려받은 국적과 재산을 생성자를 통해 바꾼 child2
-		자식 child2 = new 자식("미국", 20000);
-		child2.부모메서드();
-		child2.자식메서드();
-		
-	}
-}
+//class 부모{
+//	String 국적 = "대한민국";
+//	int 재산 = 10000;
+//	void 부모메서드() {
+//		System.out.println("저의 국적은 "+ this.국적 + ", 재산은 " + 재산 + "입니다.");
+//	}
+//}
+//class 자식 extends 부모{
+//	String 국적 = "인도";
+//	int 재산 = 50000;
+//	자식() {
+//	}
+//	자식(String 국적, int 재산){
+//		this.국적 = 국적;
+//		super.재산 = 재산;
+//	}
+//	void 자식메서드() {
+//		System.out.println("저의 국적은 "+ super.국적 + ", 재산은 " + super.재산 + "입니다.");
+//		System.out.println("저의 국적은 "+ this.국적 + ", 재산은 " + this.재산 + "입니다.");
+//	}
+//}
+//
+//public class inheritanceTest_02 {
+//	
+//	public static void main(String[] args) {
+//		//부모님의 국적과 재산을 그대로 물려받은 child1
+//		자식 child1 = new 자식();
+//		child1.부모메서드();
+//		child1.자식메서드();
+//		System.out.println("=================================");
+//		
+//		//물려받은 국적과 재산을 생성자를 통해 바꾼 child2
+//		자식 child2 = new 자식("미국", 20000);
+//		child2.부모메서드();
+//		child2.자식메서드();
+//		
+//	}
+//}
---
+++ indi/src/inheritanceTest_03.java
@@ -0,0 +1,28 @@
+class 계산기{
+	//부모 객체 메소드 선언
+	public double 원면적계산하기(double r){
+		System.out.println("계산기 객체의 원면적계산하기 실행");
+		return 3.14 * r * r;
+	}
+}
+
+class 컴퓨터 extends 계산기{
+	//자식 객체 메소드 선언
+	public double 원면적계산하기(double r) {
+		System.out.println("계산기 객체를 상속받은 컴퓨터 객체의 원면적계산하기 실행");
+		return Math.PI * r * r;
+	}
+}
+
+public class inheritanceTest_03 {
+	public static void main(String[] args) {
+		int r = 10;
+		
+		계산기 calculator = new 계산기();
+		System.out.println( "원 면적 : " + calculator.원면적계산하기(10) );
+		System.out.println();
+
+		컴퓨터 computer = new 컴퓨터();
+		System.out.println( "원 면적 : " + computer.원면적계산하기(10) );
+	}
+}
