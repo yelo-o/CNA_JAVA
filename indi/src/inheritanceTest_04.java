1cc1ceefe73d4a6451163ca50b9532fe3fec2688
yelo-o <flyordig@gmail.com>
2023-06-04 23:32:13 +0900

    다형성

---
+++ indi/src/inheritanceTest_04.java
@@ -0,0 +1,28 @@
+//class 부모 {
+//	public void 부모메소드() {
+//		System.out.println("부모 메소드 호출");
+//	}
+//	public void 오버라이딩용메소드() {
+//		System.out.println("부모 메소드 호출");
+//	}
+//}
+//class 자식 extends 부모{
+//	public void 자식메소드() {
+//		System.out.println("자식 메소드 호출");
+//	}
+//	public void 오버라이딩용메소드() {
+//		System.out.println("자식2 메소드 호출");
+//	}
+//}
+//public class inheritanceTest_04 {
+//	public static void main(String[] args) {
+//		//자식 객체 생성
+//		자식 child = new 자식();
+//		//자동 타입 변환
+//		부모 parent = child;
+//		//부모타입으로 형변환된 자식객체에서 메소드 호출
+//		parent.부모메소드();
+////		parent.자식메소드(); //안됨
+//		parent.오버라이딩용메소드();
+//	}
+//}
---
+++ indi/src/inheritanceTest_05.java
@@ -0,0 +1,24 @@
+//class 부모 {
+//	public void 부모메소드() {
+//		System.out.println("부모 메소드 호출");
+//	}
+//	public void 오버라이딩용메소드() {
+//		System.out.println("부모 메소드 호출");
+//	}
+//}
+//class 자식 extends 부모{
+//	public void 자식메소드() {
+//		System.out.println("자식 메소드 호출");
+//	}
+//	public void 오버라이딩용메소드() {
+//		System.out.println("자식2 메소드 호출");
+//	}
+//}
+//public class inheritanceTest_05 {
+//	public static void main(String[] args) {
+//		//자동 타입 변환
+//		부모 parent = new 자식();
+//		//강제 타입 변환
+//		자식 me = (자식) parent;
+//	}
+//}
