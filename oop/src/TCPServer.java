a532a67aa9e48be7b525b717670d37182375422f
yelo-o <flyordig@gmail.com>
2023-06-09 17:13:38 +0900

    클라이언트 대기열

--- oop/src/TCPServer.java
+++ oop/src/TCPServer.java
@@ -17,27 +17,29 @@ public class TCPServer {
 		try {
 			ss = new ServerSocket(port); //포트 열기
 			Socket s = null;
-			s = ss.accept(); //클라이언트의 접속을 기다린다
-			InetAddress clientIP = s.getInetAddress();
-			String clientAddress = clientIP.getHostAddress();
-			System.out.println("클라이언트 "+ clientAddress + " 가 접속했습니다");
-			is = s.getInputStream();
-			//			System.out.println((char)is.read());
-			//			byte[] bArr = new byte[100];
-			//			int readByteSize = is.read(bArr);
-			//			String receiveMsg = new String(bArr, 0, readByteSize);
-			dis = new DataInputStream(is);
-			String receiveMsg;
+			while(true) {
+				s = ss.accept(); //클라이언트의 접속을 기다린다
+				InetAddress clientIP = s.getInetAddress();
+				String clientAddress = clientIP.getHostAddress();
+				System.out.println("클라이언트 "+ clientAddress + " 가 접속했습니다");
+				is = s.getInputStream();
+				//			System.out.println((char)is.read());
+				//			byte[] bArr = new byte[100];
+				//			int readByteSize = is.read(bArr);
+				//			String receiveMsg = new String(bArr, 0, readByteSize);
+				dis = new DataInputStream(is);
+				String receiveMsg;
 //			do {
 //				receiveMsg = dis.readUTF();
 //				System.out.println("클라이언트 " + clientAddress + ">" + receiveMsg);
 //			}while(!receiveMsg.equals("quit"));
-			while(true) {
-				receiveMsg = dis.readUTF();
-				if(receiveMsg.equals("quit")) {
-					break;
+				while(true) {
+					receiveMsg = dis.readUTF();
+					if(receiveMsg.equals("quit")) {
+						break;
+					}
+					System.out.println("클라이언트 " + clientAddress + ">" + receiveMsg);
 				}
-				System.out.println("클라이언트 " + clientAddress + ">" + receiveMsg);
 			}
 			
 		} catch (IllegalArgumentException e) { //런타임에러이기 때문에 컴파일러가 체크못함
