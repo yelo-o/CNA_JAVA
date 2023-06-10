6a516b7d600bda1fb09e7ee4fb61b46d7863ab64
yelo-o <flyordig@gmail.com>
2023-06-10 18:45:13 +0900

    메인스레드는 프린팅, 작업스레드는 비프음 들려준다

--- indi/src/ch14/BeepPrintExample.java
+++ indi/src/ch14/BeepPrintExample.java
@@ -4,16 +4,24 @@ import java.awt.Toolkit;
 
 public class BeepPrintExample {
 	public static void main(String[] args) {
-		// make Toolkit instance
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		
-		for(int i=0; i<5; i++) {
-			toolkit.beep();
-			try {
-				Thread.sleep(500);
-			} catch(Exception e) {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+				for(int i=0; i<5; i++) {
+					toolkit.beep();
+					try {
+						Thread.sleep(500);
+					} catch(Exception e) {
+					}
+				}
 			}
-		}
+		});
+		
+		thread.start();
+		// make Toolkit instance
+
 
 		for(int i=0; i<5; i++) {
 			System.out.println("Thing");
