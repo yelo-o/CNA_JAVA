f6b4bda99da03f3f56238bfcb07b44896639017f
yelo-o <flyordig@gmail.com>
2023-06-10 21:00:45 +0900

    join 메소드 활용

---
+++ indi/src/ch14/JoinExample.java
@@ -0,0 +1,34 @@
+package ch14;
+
+class SumThread extends Thread {
+	private long sum;
+	
+	public long getSum() {
+		return sum;
+	}
+	
+	public void setSum(long sum) {
+		this.sum = sum;
+	}
+	
+	@Override
+	public void run() {
+		for(int i=1; i<=100; i++) {
+			sum += i;
+		}
+	}
+}
+
+public class JoinExample {
+	public static void main(String[] args) {
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		try {
+			sumThread.join();	
+		} catch (InterruptedException e) {
+		}
+		System.out.println("1~100 합: " + sumThread.getSum());
+
+	}
+
+}
