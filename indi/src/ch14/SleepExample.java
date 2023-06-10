20cafccd1b60e94b58e7b8445ecceb173e4657de
yelo-o <flyordig@gmail.com>
2023-06-10 20:46:39 +0900

    sleep() 메서드 활용

---
+++ indi/src/ch14/SleepExample.java
@@ -0,0 +1,23 @@
+package ch14;
+
+import java.awt.Toolkit;
+
+public class SleepExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for(int i=0; i<10; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(3000);
+			} catch(InterruptedException e) {
+				
+			}
+		}
+		
+			
+
+	}
+
+}
