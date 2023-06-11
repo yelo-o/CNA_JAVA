b0699880b97d6b8b074fe965b3cf68714e2ea233
yelo-o <flyordig@gmail.com>
2023-06-11 20:26:13 +0900

    알고리즘테스뚜

--- indi/src/Main.java
+++ indi/src/Main.java
@@ -1,44 +1,30 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		// Student를 저장하는 HashSet 생성
-		HashSet<Student> hashSet = new HashSet<>();
-
-		// Student 저장
-		hashSet.add(new Student("1"));
-		hashSet.add(new Student("1")); //같은 학번이므로 중복 저장이 안됨
-		hashSet.add(new Student("2"));
-
-		// 저장된 Student 수 출력
-		System.out.println("저장된 Student 수 : " + hashSet.size());
-	}
-}
-
-class Student {
-	private String studentNum;
-
-	public Student(String studentNum) {
-		this.studentNum = studentNum;
-	}
-
-	public String getStudentNum() {
-		return studentNum;
-	}
-	// 여기에 코드를 작성하세요.
-	@Override
-	public int hashCode() {
-		return studentNum.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof Student) {
-			Student student = (Student) obj;
-			if(studentNum.equals(student.getStudentNum())) {
-				return true;
+		List<Integer> lst = new ArrayList<>(); //리스트 생성
+		int x = 10;
+		lst.add(x);
+		while(x!=1) {
+			if(x%2==0) {
+				x = x/2;
+				lst.add(x);
+			}else{
+				x = 3*x + 1;
+				lst.add(x);
 			}
 		}
-		return false;
+		int[] answer = new int[lst.size()];
+		for(int i=0; i<answer.length;i++) {
+			answer[i] = lst.get(i);
+		}
+//		for(int l : lst) {
+//			answer[l] = l;
+//			
+//		}
+		for(int l : answer) {
+			System.out.println(l);
+		}
 	}
 }
