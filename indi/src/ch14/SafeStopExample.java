f90f6c75d5af38432a09dcaf7da043a7f04e0136
yelo-o <flyordig@gmail.com>
2023-06-11 18:15:14 +0900

    스레드 안전종료 - 조건 이용

---
+++ indi/src/ch14/SafeStopExample.java
@@ -0,0 +1,35 @@
+package ch14;
+
+class PrintThread extends Thread {
+	private boolean stop;
+	
+	public void setStop(boolean stop) {
+		this.stop = stop;
+	}
+	@Override
+	public void run() {
+		while(!stop) {
+			System.out.println("실행 중");
+		}
+		System.out.println("리소스 정리");
+		System.out.println("실행 종료");
+	}
+	
+}
+
+public class SafeStopExample {
+
+	public static void main(String[] args) {
+		PrintThread printThread = new PrintThread(); 
+		printThread.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+		}
+		
+		printThread.setStop(true);
+
+	}
+
+}
