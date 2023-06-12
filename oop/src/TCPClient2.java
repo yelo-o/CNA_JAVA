ff6151fee0be20dc8ad33fd6d822152d63a7d500
yelo-o <flyordig@gmail.com>
2023-06-12 09:53:08 +0900

    서버와클라이언트 연결 백업

---
+++ oop/src/TCPClient2.java
@@ -0,0 +1,57 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TCPClient2 {
+
+	public static void main(String[] args) {
+//		String serverIP = "192.168.0.40";
+		String serverIP = "127.0.0.1";
+		int serverPort = 5432;
+		Socket s = null;
+		OutputStream os = null;
+		DataOutputStream dos = null;
+		//키보드로 입력하기
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			s = new Socket(serverIP, serverPort); //직접 소켓 객체 생성
+			os = s.getOutputStream();
+			dos = new DataOutputStream(os);
+			
+//			String sendMsg = "안녕하세요. 김민규입니다";
+//			os.write(sendMsg.getBytes("UTF-8"));
+//			os.writeUTF(sendMsg);
+//			os.write(65);
+			
+			//키보드로 quit 문자열까지 서버로 send 반복한다
+			String sendMsg;
+			do {
+				sendMsg  = sc.nextLine();
+				dos.writeUTF(sendMsg);
+			}while(!sendMsg.equals("quit"));
+			
+		} catch (ConnectException e) {
+			System.out.println("서버를 찾지못했습니다");
+		} catch (SocketException e) {
+			System.out.println("서버와 연결이 해제되었습니다");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(s != null) {
+				try {
+					s.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+
+}
---
+++ oop/src/TCPServer2.java
@@ -0,0 +1,59 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class TCPServer2 {
+
+	public static void main(String[] args) {
+		ServerSocket ss = null;
+		int port = 5432; //1521;//65536;
+		InputStream is = null;
+		DataInputStream dis = null;
+		try {
+			ss = new ServerSocket(port); //포트 열기
+			Socket s = null;
+			while(true) {
+				String clientAddress = "";
+				try {
+					s = ss.accept(); //클라이언트의 접속을 기다린다
+					InetAddress clientIP = s.getInetAddress();
+					clientAddress = clientIP.getHostAddress();
+					System.out.println("클라이언트 "+ clientAddress + " 가 접속했습니다");
+					is = s.getInputStream();
+					dis = new DataInputStream(is);
+					String receiveMsg;
+					while(true) {
+						receiveMsg = dis.readUTF();
+						if(receiveMsg.equals("quit")) {
+							break;
+						}
+						System.out.println("클라이언트 " + clientAddress + ">" + receiveMsg);
+					}
+				} catch (SocketException e) {
+					System.out.println("클라이언트와의 연결이 해제되었습니다");
+				} catch (IOException e) {
+				} finally { //한 클라이언트와의 일을 끝낸다
+					System.out.println("클라이언트" + clientAddress + "접속해제");
+					if(s != null) {
+						try {
+							s.close();
+						} catch (IOException e) {
+						}
+					}
+				}
+			}
+			} catch (IllegalArgumentException e) { //런타임에러이기 때문에 컴파일러가 체크못함
+				System.out.println(port + " 포트가 틀렸어요");
+			} catch (BindException e) {
+				System.out.println(port + " 포트는 이미 사용중입니다");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("TCPSERVER END");
+		}
+	}
