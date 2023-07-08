81150e3c2f329c7f8f38c07a39de0b6628f41e4e
yelo-o <flyordig@gmail.com>
2023-07-08 19:31:20 +0900

    수제비 문제 테스트

---
+++ indi/src/Soojebi.java
@@ -0,0 +1,21 @@
+public class Soojebi {
+
+
+	public static void main(String[] args) {
+		int[] a = new int[8];
+		int i = 0;
+		int n = 10;
+		
+		while(n!=0) {
+			a[i++] = n % 2;
+			n /= 2;
+		}
+		
+		for(i = 7; i>=0; i--) {
+			System.out.print(a[i]);
+		}
+		
+
+	}
+
+}
