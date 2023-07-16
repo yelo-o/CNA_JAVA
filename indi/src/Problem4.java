51fc49cc4867550f3ec005e2dd29026b7bfda3b4
yelo-o <flyordig@gmail.com>
2023-07-16 23:41:33 +0900

    problem 4

---
+++ indi/src/Problem4.java
@@ -0,0 +1,15 @@
+public class Problem4 {
+	public static void main(String[] args) {
+		int a, b, c, sum;
+		a = b = 1;
+		sum = a + b;
+		
+		for(int i=3; i<=5; i++) {
+			c = a + b;
+			sum += c;
+			a = b;
+			b = c;
+		}
+		System.out.println(sum);
+	}
+}
