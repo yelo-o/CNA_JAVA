bfcf27ccaeecf1f8556781612aad3a2e2dcf89c2
yelo-o <flyordig@gmail.com>
2023-07-16 23:21:13 +0900

    promblem 3

--- indi/src/Problem2.java
+++ indi/src/Problem2.java
@@ -1,33 +1,33 @@
-class Parent{
-	public Parent() {
-		System.out.print("A");
-	}
-	public Parent(int a) {
-		System.out.print("B");
-	}
-	public void fn() {
-		System.out.print("C");
-	}
-}
-class Child extends Parent{
-	public Child() {
-		System.out.print("D");
-	}
-	public Child(int a) {
-		super(a);
-		System.out.print("E");
-	}
-	public Child(double a) {
-		System.out.print("F");
-	}	
-	public void fn() {
-		System.out.print("G");
-	}
-}
-public class Problem2 {
-	public static void main(String[] args) {
-		Parent a = new Child();
-		Child b = new Child();
-		Child c = new Child(5);
-	}
-}
+//class Parent{
+//	public Parent() {
+//		System.out.print("A");
+//	}
+//	public Parent(int a) {
+//		System.out.print("B");
+//	}
+//	public void fn() {
+//		System.out.print("C");
+//	}
+//}
+//class Child extends Parent{
+//	public Child() {
+//		System.out.print("D");
+//	}
+//	public Child(int a) {
+//		super(a);
+//		System.out.print("E");
+//	}
+//	public Child(double a) {
+//		System.out.print("F");
+//	}	
+//	public void fn() {
+//		System.out.print("G");
+//	}
+//}
+//public class Problem2 {
+//	public static void main(String[] args) {
+//		Parent a = new Child();
+//		Child b = new Child();
+//		Child c = new Child(5);
+//	}
+//}
