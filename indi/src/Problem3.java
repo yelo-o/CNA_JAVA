cf770c022e9ffc07c6477be3cdb2c96efb1c8859
yelo-o <flyordig@gmail.com>
2023-07-17 23:48:10 +0900

    daily problem5

--- indi/src/Problem3.java
+++ indi/src/Problem3.java
@@ -1,19 +1,19 @@
-class Parent{
-	int a = 0;
-	Parent(){
-		a++;
-		System.out.print(a);
-	}
-}
-class Child extends Parent{
-	int b = 0;
-	Child(int a){
-		this.b = a;
-		System.out.print(a++);
-	}
-}
-public class Problem3 {
-	public static void main(String[] args) {
-		Parent pa = new Child(5);
-	}
-}
+//class Parent{
+//	int a = 0;
+//	Parent(){
+//		a++;
+//		System.out.print(a);
+//	}
+//}
+//class Child extends Parent{
+//	int b = 0;
+//	Child(int a){
+//		this.b = a;
+//		System.out.print(a++);
+//	}
+//}
+//public class Problem3 {
+//	public static void main(String[] args) {
+//		Parent pa = new Child(5);
+//	}
+//}
---
+++ indi/src/Problem5.java
@@ -0,0 +1,19 @@
+class Parent {
+	String className = "Parent";
+	public void info() {
+		System.out.print(className);
+	}
+}
+class Child extends Parent {
+	String className = "Child";
+	public void info() {
+		super.info();
+		System.out.print(className);
+	}
+}
+public class Problem5 {
+	public static void main(String[] args) {
+		Parent p = new Child();
+		p.info();
+	}
+}
