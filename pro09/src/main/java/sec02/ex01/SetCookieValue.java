71105fcc01a8c8993d0fe8f4a0363383b66eacb3
yelo-o <flyordig@gmail.com>
2023-07-30 21:08:35 +0900

    쿠키 작동 시간 변경

--- pro09/src/main/java/sec02/ex01/SetCookieValue.java
+++ pro09/src/main/java/sec02/ex01/SetCookieValue.java
@@ -21,7 +21,8 @@ public class SetCookieValue extends HttpServlet {
 		Date d = new Date();
 		
 		Cookie c = new Cookie("cookieTest", URLEncoder.encode("JSP프로그래밍입니다.","utf-8"));
-		c.setMaxAge(24*60*60);
+		//c.setMaxAge(24*60*60);
+		c.setMaxAge(-1);
 		response.addCookie(c);
 		
 		out.println("현재시간 : " + d);
